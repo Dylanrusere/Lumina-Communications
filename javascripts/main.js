ea3d4721b56efaecca6ba6929a4badbb6710fbe2
dylanrusere <dylaneight01@gmail.com>
2025-04-29 13:57:59 +0200

    updates

--- javascripts/main.js
+++ javascripts/main.js
@@ -42,7 +42,7 @@ const services = [
 
 const backgrounds = [
     "url('../assets/images/hero.jpg')",
-    "url('../assets/images/img24.jpg')",
+    "url('../assets/images/about-page.jpg')",
     "url('../assets/images/hero.jpg')",
     "url('../assets/images/img24.jpg')",
     "url('../assets/images/hero.jpg')"
@@ -80,3 +80,32 @@ function changeBackground() {
 // Start typing effect and background change
 typeText(services[textIndex]);
 setInterval(changeBackground, 5000);
+
+
+// Portfolio
+function filterProjects(category, buttonId) {
+    const projectCards = document.querySelectorAll('.project-card');
+    const filterButtons = document.querySelectorAll('.filter-btn');
+
+    // Show or hide projects based on selected category
+    projectCards.forEach((card) => {
+      const projectCategory = card.getAttribute('data-category');
+
+      if (category === 'All' || projectCategory === category) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+
+    // Remove active class from all buttons
+    filterButtons.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+
+    // Add active class to the clicked button
+    document.getElementById(buttonId).classList.add('active');
+  }
+
+  // Default to showing all projects on page load
+  filterProjects('All', 'all');
